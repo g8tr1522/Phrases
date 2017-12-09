5fe6a12f3434a91bec4ecfb2e0eeb2f40ca7625e
William Prophet <g8tr1522@gmail.com>
2017-12-09 01:33:27 -0500

    changed 'phrases/phrases.lua' so s work in xStream

--- phrases.lua
+++ phrases.lua
@@ -1,13 +1,36 @@
 phrases = {}
 
-phrases.ins 		= require('phrases.ins')
-phrases.mDelays = require('phrases.mDelays')
+
+---- Deprecated require style which relies on main being in the level above this file.
+-------------------------------------------------------------------------------
+-- __mainroot = 'phrases.' --this is the location of the main file that requires this file
+
+-- phrases.ins 		= require(__mainroot..'ins')
+-- phrases.mDelays = require(__mainroot..'mDelays')
+-- --phrases.mNotes = require('phrases.mNotes')
+
+-- phrases.utils 	= require(__mainroot..'utils')
+-- --phrases.utils.iters = require('phrases.utils.iters')
+
+-- phrases.tabler 	= require(__mainroot..'tabler')
+
+
+
+---- 'safer' require style, which relies on main being where the `..` is in the following statement
+---- This style is appropriate if the Phrases submodule repo lies in the root directory of xStream.
+-------------------------------------------------------------------------------
+package.path = package.path .. ';../Phrases/phrases/?.lua'
+
+phrases.ins 		= require('ins')
+phrases.mDelays = require('mDelays')
 --phrases.mNotes = require('phrases.mNotes')
 
-phrases.utils 	= require('phrases.utils')
+phrases.utils 	= require('utils')
 --phrases.utils.iters = require('phrases.utils.iters')
 
-phrases.tabler 	= require('phrases.tabler')
+phrases.tabler 	= require('tabler')
+
+
 
 
 
