9ccc4388965fa94a7b22e7dfabd1853c04b17487
William Prophet <g8tr1522@gmail.com>
2017-12-16 15:58:18 -0500

    deleted portions of Chance/chance.lua
    This included the people, time, web, and poker subfunctions. Also, I moved the library as a key in Phrases, so the library may be called with

--- tests.lua
+++ tests.lua
@@ -1,26 +1,26 @@
 -- This is just used to test out new code. This file isn't required.
 print()
 Phrases = require('Phrases')
+chance	= require('Chance/chance')
 
+-- obj = Phrases.Ins:new{subtype="mp_lead", top=5, nopl=64, nonp=3}
+-- obj:print_info()
 
-obj = Phrases.Ins:new{subtype="mp_lead", top=5, nopl=64, nonp=3}
-obj:print_info()
-
-obj.set:notes{10,20,30,40}
-obj.set:notes(2, {12.5,22.5,32.5,42.5})
-obj.set:notes(3, {15,25,35,45})
-obj.set:delays{1,2.5,3,3.5}
-obj:print_info()
+-- obj.set:notes{10,20,30,40}
+-- obj.set:notes(2, {12.5,22.5,32.5,42.5})
+-- obj.set:notes(3, {15,25,35,45})
+-- obj.set:delays{1,2.5,3,3.5}
+-- obj:print_info()
 
 
 
 -- require('Phrases.utils.forgroup')
 
--- g = {
-	-- {10,20,30},
-	-- {11,21,31},
-  -- {5,15,25,35,45},
--- }
+g = {
+	{10,20,30},
+	{11,21,31},
+  {5,15,25,35,45},
+}
 
 -- rg = forgroup(g, {3}, {5}, 
 	-- function (v, i, g) 
