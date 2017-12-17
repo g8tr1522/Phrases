7d117365ab700465fd7afdf734bc10590b6bd1fc
William Prophet <g8tr1522@gmail.com>
2017-12-17 16:21:26 -0500

    fixed a bug in shake.lua. Improved argument handling
    Also improved documentation.

--- Phrases/DelaysMethods/shake.lua
+++ Phrases/DelaysMethods/shake.lua
@@ -1,46 +1,52 @@
---DelaysMethods.shake
---Shift (or shake) the delay of note_N by at least (the third arg)
+--DelaysMethods.shake{ min_delay, Vsel, shuffle }
+--Shift (or shake) a table of delays by at least `min_delay`
 
 --[=[ mini documentation
-- arguments (only one optional arg):
-		note_N (integer or table): 
-			We will shift the Nth note/delay value by at least oparg1.
-			If note_N is a table with a single value {n}, then we will only shift note n.
-			If note_N is a table with several values {n1, n2, n3, ...}, 
-				then we will shift all of those notes, 
-				where their bounds are obtained from the initial version of dels_tt.
-			If note_N is an integer M, 
-				then we will randomly pick M different delays, and shift those notes.
-			If note_N = -1, then shift the delays of all notes.
-		oparg1 (float) (default = 0.25): 
-			The minimum amount of delay to shift by. 
-			Must be in traditional time.
-			AKA min_delay in implementation.
+
+- arguments :
+		Note that arguments are provided in a table.
+		- min_delay (number) (default = 0.25) : 
+				The minimum delay amount to jump by.
+		- Vsel (table, number, or string) (default = "all"):
+				If a table, then the elements in Vsel correspond to indices in the delays table 
+					to perform the algorithm on.
+				If a number, then perform the algorithm on that many elements (randomly selected).
+				If a string, then provide the string "all" to shake all elements.
+		- shuffle (boolean) (default = true) :
+				If true, then shuffle whatever Vsel was.
+				
 - explanation:
 		If we want the fourth note to be shifted by at least an eigth note, 
 			then we call foo like this:
-			obj:shake_delay({4}, 0.125)
+			obj:shake_delay{0.125, {4}}
+			
 - example:
-		Lets say our dels_tt table has 5 notes, 
-			and the traditional delays ("dels_traditional") are
-			{1,2, 3.25,3.75,4}.
-		We call foo just like above: 
-			obj:shake_delay({4}, 0.125)
+		Lets say object.delays.tn has 5 notes :
+			{1, 2, 3.25, 3.75, 4}.
+		We call shake just like above: 
+			obj:shake_delay{0.125, {4}}
 		The traditional delays in the new version of dels_tt table will then be:
 			{1, 2, 3.25, X, 4}
 			where X could be any of these randomly chosen values:
 			X : [3.375, 3.5, 3.625, 3.75, 3.875]
-- notes:
-		- About boundaries of possible delays:
+			
+- About boundaries of possible delays:
 		Delays are 'stuck between' the notes next to them.
-		This is because mutate functions are only intended for monophonic melodies. 
-		In the example, X could not have been (e.g.) 4.0 or 4.125 because note 5 has a delay of 4.
-		This is why the function is called 'shake_delay'
-- Future improvements: 
+		This is because an Ins object is 'monophonic' in a sense. 
+		In the example, X could not have been (e.g.) 4.0 or 4.125 because 
+				note 5 has a delay of 4.
+		This is why the function is called 'shake' - imagine shaking 
+				a tube with large rocks in it - the rocks could not slip 
+				past each other no matter how hard you shook the tube.
+				Thus, the notes can not be shaken past each other.
+		
+- future improvements: 
 		Allow a 2nd oparg which specifies whether 
 			oparg1 (the minimum delay) is in traditional time, or in pattern lines.
 		Will multiple calls to math.random() generate different results, even if the seed is set before calling foo?
 			If so, then maybe 'pause' the seed at the beginning of the call, and 'resume' it at the end.
+			Or provide additional arguments to set seeds to certain `chance` calls.
+		Allow notes to shake past each other. Use clipboard functions to accomplish this.
 		
 --]=]---[=[--
 
@@ -59,18 +65,13 @@ shake = function (self, argt)
 	--`temp` is now the table this function operates on
 	
 --handle arguments table
-
-	--minimum delay jump
-	if not argt.min_delay then
-		argt.min_delay = 0.25
-	end
+	--assign unnamed args to proper key names
+	argt.min_delay = argt.min_delay or argt[1] or 0.25
+	argt.Vsel      = argt.Vsel or argt[2] or "all"
+	argt.shuffle	 = argt.shuffle or argt.random or argt[3] or true
 	
 	--Vsel 
-	local Vsel_was_number = false
-	
 	if type(argt.Vsel)=="number" then
-		--Vsel_was_number = true
-		--argt.Vsel = {argt.Vsel}
 		local number = argt.Vsel
 		argt.Vsel = Phrases.tabler.idx_shuffled(#temp)
 		if number <= #temp then
@@ -84,10 +85,9 @@ shake = function (self, argt)
 					.."  = Argument Vsel should be a table of numbers."
 					.."  = These numbers will correspond to the indices to operate over.", 2)
 		end
-	elseif type(argt.Vsel)=="string" then
-		if argt.Vsel=="all" then
+	elseif argt.Vsel=="all" then
 			argt.Vsel = Phrases.tabler.idx( #temp )
-		end
+		--end
 	else
 		error("=== Error in DelaysMethods.shake"
 				.."  = Argument Vsel should be either ..."
@@ -97,7 +97,7 @@ shake = function (self, argt)
 	end 
 	
 	--shuffle Vsel
-	if (argt.shuffle==true) or (argt.shuffle==nil) then
+	if argt.shuffle==true then
 		local chance = require('Chance.chance')
 		argt.Vsel = chance.helpers.shuffle(argt.Vsel)
 	end
@@ -105,22 +105,24 @@ shake = function (self, argt)
 	
 --main part here
 	for _,v in ipairs(argt.Vsel) do
-		local dmin = 0
-		local dmax = 0
-		
-		if v==1 then
-			dmin = 1
-			dmax = temp[2] or self.delays.top
-		elseif v==#temp then
-			dmin = temp[#temp-1] or 1
-			dmax = self.delays.top
-		else
-			dmin = temp[v-1]
-			dmax = temp[v+1]
+		if (v>0) and (v <= #temp) then
+			local dmin = 0
+			local dmax = 0
+			
+			if v==1 then
+				dmin = 1 - argt.min_delay		-- see assignment to `possibly` 
+				dmax = temp[2] or self.delays.top
+			elseif v==#temp then
+				dmin = temp[#temp-1] or 1
+				dmax = self.delays.top
+			else
+				dmin = temp[v-1]
+				dmax = temp[v+1]
+			end
+			
+			possibly = range(dmin+argt.min_delay, dmax-argt.min_delay, argt.min_delay, temp[v])
+			temp[v] = possibly[ math.random(#possibly) ]
 		end
-		
-		possibly = range(dmin, dmax, argt.min_delay, temp[v])
-		temp[v] = possibly[ math.random(#possibly) ]
 	end
 	
 	self.set:delays(temp)
